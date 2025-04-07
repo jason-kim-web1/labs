3a47323250d1749d2c28c10b86b936c0b89a8da8
jason-kim-web1 <jason.kim@kurlycorp.com>
2025-04-07 17:00:16 +0900

    fix: 불필요한 코드 제거(https만 필요)

--- nx-proxy/dev-server.js
+++ nx-proxy/dev-server.js
@@ -1,37 +1,23 @@
 const { parse } = require('url');
 const next = require('next');
 
-const port = parseInt(process.env.PORT, 10);
 const app = next({ dev: true });
 const handle = app.getRequestHandler();
 
-if (port === 443) {
-  const { createServer } = require('https');
-  const fs = require('fs');
+const { createServer } = require('https');
+const fs = require('fs');
 
-  app.prepare().then(() => {
-    createServer(
-      {
-        key: fs.readFileSync('./certs/key.pem'),
-        cert: fs.readFileSync('./certs/cert.pem'),
-      },
-      (req, res) => {
-        const parsedUrl = parse(req.url, true);
-        handle(req, res, parsedUrl);
-      }
-    ).listen(port);
-   
-    console.log('> HTTPS Server listening');
-  });
-} else {
-  const { createServer } = require('http');
-
-  app.prepare().then(() => {
-    createServer((req, res) => {
-      const parsedUrl = parse(req.url, true)
-      handle(req, res, parsedUrl)
-    }).listen(port);
+app.prepare().then(() => {
+  const server = createServer(
+    {
+      key: fs.readFileSync('./certs/key.pem'),
+      cert: fs.readFileSync('./certs/cert.pem'),
+    },
+    (req, res) => {
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl);
+    }
+  ).listen(443);
   
-    console.log('> HTTP Server listening');
-  })
-}
+  console.log('> HTTPS Server listening');
+});
